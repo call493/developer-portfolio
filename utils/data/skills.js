10ecc23b502113d296592d9b55edce2adfc7c1f8
Mandara <113437397+call493@users.noreply.github.com>
2024-11-18 19:26:09 +0300

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -2,7 +2,6 @@ export const skillsData = [
   'HTML',
   'CSS',
   'Javascript',
-  'Typescript',
   'React',
   'Next JS',
   'Tailwind',
@@ -13,12 +12,11 @@ export const skillsData = [
   'AWS',
   'Bootstrap',
   'Docker',
-  'Go',
   'Figma',
+  'Photoshop',
+  'Illustrator'
   'Firebase',
-  'MaterialUI',
-  'Nginx',
-  'Strapi'
+  'MaterialUI'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
