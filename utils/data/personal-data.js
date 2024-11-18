96010518add55d0e3a7fa5f63d0a146272d40d0f
Mandara <113437397+call493@users.noreply.github.com>
2024-11-18 18:57:23 +0300

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,17 +1,14 @@
 export const personalData = {
-  name: "ABU SAID",
+  name: "BRAXTON MANDARA",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "My name is ABU SAID. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'abusaid7388@gmail.com',
-  phone: '+8801608797655',
-  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  description: "My name is Braxton Mandara. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'braxtonmandara254@gmail.com',
+  phone: '+254114391130',
+  address: 'Nyeri, Kenya ',
+  github: 'https://github.com/call493',
+  facebook: 'https://www.facebook.com/',
+  linkedIn: 'https://www.linkedin.com/in/braxton-mandara-480720214/',
+  twitter: 'https://twitter.com/',
+  resume: "https://docs.google.com/document/d/1OCj8Ze3oiqywQ31OuiQUPJmvOVsJHW7z-NPtWr2xaD8/edit?usp=sharing"
 }
